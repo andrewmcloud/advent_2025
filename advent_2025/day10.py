e613b0cd1ff2978ec2cd3e1a2d418cb231b81e4b
Andrew McLoud <amcloud@gmail.com>
2025-12-11 07:01:30 -0500

    day10 cleanup

--- advent_2025/day10.py
+++ advent_2025/day10.py
@@ -4,7 +4,8 @@ from itertools import permutations
 with open("../input/day10.txt") as f:
     machines = [line.strip("\n") for line in f.readlines()]
 
-def parse_machine(machine: str) -> tuple[list[int], list[list[int]]]:
+
+def parse_machine(machine: str) -> tuple[int, list[int]]:
     indicator = [0 if x == "." else 1 for x in re.findall(r"[\\.#]+", machine)[0]]
     schematics = [[int(y) for y in x.split(",")] for x in re.findall(r"(?<=\().*?(?=\))", machine)]
     wirings = []
@@ -12,17 +13,17 @@ def parse_machine(machine: str) -> tuple[list[int], list[list[int]]]:
         wiring = [0]*len(indicator)
         for s in schematic:
             wiring[s] = 1
-        wirings.append(wiring)
-    return indicator, wirings
+        wirings.append(int("".join(str(x) for x in wiring), 2))
+    return int("".join(str(x) for x in indicator), 2), wirings
 
 
-def press_buttons(indicator: list[int], wirings: list[list[int]]) -> int:
+def press_buttons(indicator: int, wirings: list[int]) -> int:
     presses = 1
     while True:
         for buttons in permutations(wirings, presses):
-            state = [0]*len(indicator)
+            state = 0
             for button in buttons:
-                state = [x ^ y for x, y in zip(state, button)]
+                state ^= button
             if state == indicator:
                 return presses
         presses += 1
