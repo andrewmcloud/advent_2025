2efa5e909fd9acaa9c234765dcb29f9a1ad81198
Andrew McLoud <amcloud@gmail.com>
2025-12-11 21:31:57 -0500

    day11 part 2

--- advent_2025/day11.py
+++ advent_2025/day11.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+from functools import cache
 
 with open("../input/day11.txt") as f:
     lines = f.readlines()
@@ -10,11 +11,11 @@ for line in lines:
         graph[parent].add(child.strip("\n"))
 
 
-def find_paths(graph, start: str, end: str) -> int:
+def find_paths(graph: dict[str, list[str]], start: str, end: str) -> int:
     paths = []
     def search(node, path):
         path = path + [node]
-        if node == end or node not in graph:
+        if node == end:
             paths.append(path)
             return
         for neighbor in graph[node]:
@@ -24,4 +25,19 @@ def find_paths(graph, start: str, end: str) -> int:
     return len(paths)
 
 
+def find_paths_contains(graph: dict[str, list[str]], start: str, end: str) -> int:
+    @cache
+    def search(node: str, dac: bool = False, fft: bool = False) -> int:
+        if node == end:
+            return 1 if (dac and fft) else 0
+        if node == "dac":
+            dac = True
+        elif node == "fft":
+            fft = True
+
+        return sum(search(neighbor, dac, fft) for neighbor in graph[node])
+    return search(start)
+
+
 print(f"part 1: {find_paths(graph, 'you', 'out')}")
+print(f"part 2: {find_paths_contains(graph, 'svr', 'out')}")
